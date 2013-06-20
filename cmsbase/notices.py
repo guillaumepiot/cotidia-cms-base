8606b4e312e9ba2ea1f46d37f8a625b8ac1d3819
Guillaume Piot <guillaume@viva-aspire.com>
2013-06-20 10:14:07 +0100

    Updated setup file to include all non-py files

--- cmsbase/notices.py
+++ cmsbase/notices.py
@@ -1,16 +1,16 @@
-	from cotimail import Notice
+from cotimail import Notice
 
-	class CustomNotice(Notice):
-		# Use as a list display
-		name = 'Moderation request' 
-		# Use for the preview URL as a slug, so it must not contains spaces or other symbols than lowercase letters and hyphens
-		identifier = 'moderation-request' 
-		# Defines an HTML template for this notice
-		html_template = 'cmsbase/notice/moderate.html'
-		text_template = 'cmsbase/notice/moderate.txt'
+class CustomNotice(Notice):
+	# Use as a list display
+	name = 'Moderation request' 
+	# Use for the preview URL as a slug, so it must not contains spaces or other symbols than lowercase letters and hyphens
+	identifier = 'moderation-request' 
+	# Defines an HTML template for this notice
+	html_template = 'cmsbase/notice/moderate.html'
+	text_template = 'cmsbase/notice/moderate.txt'
 
-		# A JSON representation of the context dictionary, which is the format it will be saved as in the EmailLog
-		context = {'object-title': 'Object title'}
-		
-		# Passing on come context variables to build the subject line 
-		subject = u'%s %s' % ('Moderation request for: ', context['object-title'])
+	# A JSON representation of the context dictionary, which is the format it will be saved as in the EmailLog
+	context = {'object-title': 'Object title'}
+	
+	# Passing on come context variables to build the subject line 
+	subject = u'%s %s' % ('Moderation request for: ', context['object-title'])
